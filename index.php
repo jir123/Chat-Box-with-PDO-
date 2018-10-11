3586588746d42898c6c20e92275fa8986914b01e
Jahirul Islam Razu <jirrazu123@gmail.com>
2018-10-11 22:11:16 +0600

    Desigh template

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ChatBox</title>
+    <link rel="stylesheet" href="styles.css"> 
+</head>
+<body>
+    <div class="container">
+        <div class="chat-box">
+            <ul>
+                <li><span class="time">11 Oct 2018</span><span class="name">Jahirul Islam Razu</span><span class="chat-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, unde.</span></li>
+                <li><span class="time">11 Oct 2018</span><span class="name">Jahirul Islam Razu</span><span class="chat-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, unde.</span></li>
+                <li><span class="time">11 Oct 2018</span><span class="name">Jahirul Islam Razu</span><span class="chat-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, unde.</span></li>
+                <li><span class="time">11 Oct 2018</span><span class="name">Jahirul Islam Razu</span><span class="chat-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, unde.</span></li>
+                <li><span class="time">11 Oct 2018</span><span class="name">Jahirul Islam Razu</span><span class="chat-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, unde.</span></li>
+            </ul>
+        </div>
+        <div class="chat-from">
+            <form action="">
+            <table>
+                <tr>
+                    <td><input type="text" name="name" placeholder="Your Name" required/></td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="body" placeholder="Your Comment ... " required/></td>
+                </tr>
+                <tr>
+                    <td><input type="submit"  vlaue="Shout It"/></td>
+                </tr>
+            </table>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
